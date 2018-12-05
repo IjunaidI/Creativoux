f747199c215d7b86ab9ef7985f94aca7d77c0193
IjunaidI <mjunaid7843@gmail.com>
2018-12-05 11:31:17 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+let userDetails = {
+	profileImage: "images/profile-image.png",
+	portfolioImages: ["","",""],
+	applyDetail: function(){
+		document.querySelector(".profile-image").style.backgroundImage = `url(${this.profileImage})`
+	},
+}
+userDetails.applyDetail();
+
+
+
+
+
+
+
 function changePage(pageNum,tabNum){
     for (i = 1; i < 5; i++) {
 		document.querySelector(`.tab${i}`).classList.remove('active');
@@ -7,5 +22,9 @@ function changePage(pageNum,tabNum){
     }
 		document.querySelector(`.${tabNum}`).classList.add('active');
         document.querySelector(`.${pageNum}`).classList.add('display-block');    
-        
+}
+
+function switchMenu(){
+	document.querySelector('.hamburger-wrap').classList.toggle('display-block');
+	document.querySelector('.main-nav').classList.toggle("overlay-dark");
 }
